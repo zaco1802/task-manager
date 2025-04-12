32c63e56e4faba920d2475222db14020a09cd645
Zachery <zachery.govender@gmail.com>
2025-04-12 14:57:06 +0200

    commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let modal = document.querySelector(".modal");
+let onSubmit = document.getElementById("onSubmit");
+
+console.log(onSubmit)
+
+onSubmit.addEventListener("submit", function () {
+        
+        modal.style.display = "flex"
+        event.preventDefault()
+        // alert("hey you just click!");
+})
+ 
+
+
+
+
